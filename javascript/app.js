c8f566e3ca9f8cfd5602fc2a0b1fa33dd909556d
Marisa Cassidy <marisancassidy@gmail.com>
2021-08-16 17:35:14 +0900

    Modify remove button

--- javascript/app.js
+++ javascript/app.js
@@ -1,31 +1,53 @@
 const btnCreate = document.querySelector('.btn-main');
 const btnToggle = document.querySelector('.btn-toggle');
 const btnRemove = document.querySelector('.btn-remove');
+const taskList = document.querySelector(".list-container ul");
+const listItems = taskList.children;
 
-btnCreate.addEventListener('click', () => {												 
-  const input = document.querySelector('.input-main');
-	const list = document.querySelector('ul');
-
-	list.insertAdjacentHTML(
-		'afterbegin',
-		`<li>${input.value}</li>`
-	);
-  input.value = '';
+taskList.addEventListener("click", (event) => {
+	if (event.target.tagName == "BUTTON") {
+		let button = event.target;
+		let li = button.parentNode;
+		li.remove();
+	}
 });
 
-btnToggle.addEventListener('click', () => {
-  const listContainer = document.querySelector('.list-container');
-
-	if (listContainer.style.display === 'none') {
-		btnToggle.textContent = 'Hide List';
-		listContainer.removeAttribute('style');
+btnToggle.addEventListener("click", () => {
+	const listContainer = document.querySelector(".list-container");
+	if (listContainer.style.display === "none") {
+		btnToggle.textContent = "Hide List";
+		listContainer.removeAttribute("style");
 	} else {
-		btnToggle.textContent = 'Show List';
-		listContainer.style.display = 'none'; 	
-	}		
+		btnToggle.textContent = "Show List";
+		listContainer.style.display = "none";
+	}
 });
 
-btnRemove.addEventListener('click', () => {
-	const lastItem = document.querySelector('li:last-child');
-	lastItem.remove();
+btnCreate.addEventListener("click", () => {
+	let ul = document.getElementsByTagName("ul")[0];
+	const input = document.querySelector(".input-main");
+	let li = document.createElement("li");
+	li.textContent = input.value;
+	ul.appendChild(li);
+	attachRemoveButton(li);
+	input.value = "";
 });
+
+function attachRemoveButton(li) {
+	let remove = document.createElement("button");
+	remove.className = "remove";
+	remove.textContent = "Remove";
+	li.appendChild(remove);
+}
+
+for (let i = 0; i < listItems.length; i++) {
+	attachRemoveButton(listItems[i]);
+}
+
+taskList.addEventListener("click", (event) =>  {
+	if (event.target.tagName === "BUTTON") {
+		const button = event.target;
+		const li = button.parentNode;
+		li.remove();
+	}
+})
