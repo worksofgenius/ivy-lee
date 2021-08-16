d576dd4c4696673183f302891fa82eeb60f8e862
Marisa Cassidy <marisancassidy@gmail.com>
2021-08-16 15:51:16 +0900

    Add javascript

---
+++ javascript/app.js
@@ -0,0 +1,25 @@
+const btnCreate = document.querySelector('.btn-main');
+const btnToggle = document.querySelector('.btn-toggle');
+
+btnCreate.addEventListener('click', () => {												 
+  const input = document.querySelector('.input-main');
+	const list = document.querySelector('ul');
+
+	list.insertAdjacentHTML(
+		'afterbegin',
+		`<li>${input.value}</li>`
+	);
+  input.value = '';
+});
+
+btnToggle.addEventListener('click', () => {
+  const listContainer = document.querySelector('.list-container');
+
+	if (listContainer.style.display === 'none') {
+		btnToggle.textContent = 'Hide List';
+		listContainer.removeAttribute('style');
+	} else {
+		btnToggle.textContent = 'Show List';
+		listContainer.style.display = 'none'; 	
+	}		
+});
